91d3ab89cc9300b7d5228b81020d2fef27e6c34e
fruniia <annanilssonniia@gmail.com>
2023-05-25 09:48:31 +0200

    Added summary UserThreadManager

--- GameForum1/DAL/UserThreadManager.cs
+++ GameForum1/DAL/UserThreadManager.cs
@@ -1,16 +1,19 @@
 ﻿namespace GameForum1.DAL
 {
+    /// <summary>
+    /// Gets UserThreads from API
+    /// </summary>
     public class UserThreadManager
     {
         private static Uri _baseAdress = new Uri("https://gamersparadiseapi.azurewebsites.net/");
-
         public static List<UserThread> UserThreads { get; set; }
         public static UserThread UserThread { get; set; }
 
 
-        // API
-
-        // GET
+        /// <summary>
+        /// Get all userthreads
+        /// </summary>
+        /// <returns>List of UserThreads</returns>
         public static async Task<List<UserThread>> GetUserThreads()
         {
             UserThreads ??= new List<UserThread>();
@@ -30,7 +33,11 @@
             return UserThreads;
         }
 
-        // GET ONE (ID)
+        /// <summary>
+        /// Get one specific userthread
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>One Userthread</returns>
         public static async Task<UserThread> GetOneUserThread(int id)
         {
             UserThreads ??= new List<UserThread>();
@@ -51,7 +58,11 @@
             return UserThread;
         }
 
-        // UPDATE
+        /// <summary>
+        /// Update an userthread 
+        /// </summary>
+        /// <param name="existingUserThread"></param>
+        /// <returns></returns>
         public static async Task UpdateUserThread(UserThread existingUserThread)
         {
             
@@ -65,7 +76,11 @@
             
         }
 
-        // CREATE
+        /// <summary>
+        /// Create a userthread
+        /// </summary>
+        /// <param name="existingUserThread"></param>
+        /// <returns></returns>
         public static async Task CreateUserThread(UserThread existingUserThread)
         {
             UserThreads ??= await GetUserThreads();
@@ -79,7 +94,11 @@
             }
         }
 
-        // DELETE
+        /// <summary>
+        /// Delete one userthred
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public static async Task DeleteUserThread(int id)
         {
             using (var client = new HttpClient())

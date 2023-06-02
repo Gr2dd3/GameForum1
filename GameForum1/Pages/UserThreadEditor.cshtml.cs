442c4e98fbebf57b401f7756c5b479c1fe5824ed
fruniia <annanilssonniia@gmail.com>
2023-06-02 11:53:18 +0200

    Added UserThreadEditor

--- GameForum1/Pages/UserThreadEditor.cshtml.cs
+++ GameForum1/Pages/UserThreadEditor.cshtml.cs
@@ -1,36 +1,33 @@
 namespace GameForum1.Pages;
 
-public class CommentEditorModel : PageModel
+public class UserThreadEditorModel : PageModel
 {
     private readonly UserManager<GameForum1User> _userManager;
 
-    public CommentEditorModel(UserManager<GameForum1User> userManager)
+    public UserThreadEditorModel(UserManager<GameForum1User> userManager)
     {
-        Comment = new();
+        UserThread = new();
         _userManager = userManager;
     }
 
     public GameForum1User MyUser { get; set; }
     public GameForum1User AuthorUser { get; set; }
     [BindProperty]
-    public Comment Comment { get; set; }
     public UserThread UserThread { get; set; }
+
+    public SubCategory SubCategory { get; set; }
     public string ImageSrc { get; set; }
 
     public async Task<IActionResult> OnGetAsync(int editId)
     {
-        Comment = await DAL.CommentManager.GetOneComment(editId);
+        UserThread = await DAL.UserThreadManager.GetOneUserThread(editId);
 
         return Page();
-
     }
     public async Task<IActionResult> OnPostEditAsync()
     {
-       
-        await DAL.CommentManager.UpdateComment(Comment);
+        await DAL.UserThreadManager.UpdateUserThread(UserThread);
 
-        return RedirectToPage("./Comments", new { UserThreadId = Comment.UserThreadId });
-
+        return RedirectToPage("./Userthreads", new { SubCategoryId = UserThread.SubCategoryId });
     }
-
 }
